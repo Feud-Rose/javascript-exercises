1d4d98dea6af0e9c1aff6caf53805b6a9ada2771
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-10 15:12:49 -0600

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayTarget, ...removeVal ) {
+for ( i = 0; i < arrayTarget.length; i++) {
+        let checkThis = arrayTarget[i];
+        for ( j = 0; j < removeVal.length; j++) {
+            let removeThis = removeVal[j];
+            if (removeThis === checkThis) {
+                arrayTarget.splice(i, 1);
+                i--
+            }
+        }
+    }
+   
+    return arrayTarget
+    
+    
+    
+ };
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
+  
+//if (checkThis === a) {
+//    arrayTarget.splice(i, 1)
+//}
+//else if (checkThis === b) {
+//    arrayTarget.splice(i, 1)
+//}
