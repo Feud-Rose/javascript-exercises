d01c23a2dfe37cfe300ef90fa42c63f402df926e
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-10 13:31:36 -0600

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str1) {
+let splitString = str1.split("");
+let revArray = splitString.reverse();
+let result = revArray.join("");
+return result
 };
 
 // Do not edit below this line
