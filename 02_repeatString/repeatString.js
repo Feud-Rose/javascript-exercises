c3cf792f8909f0063fa0051f009eb8a39e414465
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-10 13:16:04 -0600

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
-};
+const repeatString = function(a,b) {
+    let str1 = ""
+    if (b < 0) {
+        return "ERROR"
+    }
+    for (i = 0; i < b; i++) {
+    str1 += a;
+    }
+    return str1
+    } 
+
 
 // Do not edit below this line
 module.exports = repeatString;
