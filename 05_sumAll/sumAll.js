9dcfeaadf7729becfb66b15a6795b153a843b463
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-11 14:33:33 -0600

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a, b) {
+    if (typeof a !== "number") {
+        return "ERROR"
+    }
+    else if (typeof b !== "number") {
+        return "ERROR"
+    }
+    else if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    let result = 0
+    if (a < b) {
+        for (i=0; i < (b-a)+1; i++) {
+            let int1 = a + i
+            result += int1
+    }
+    }
+    else if (b < a) {
+        for (i=0; i < (a-b)+1; i++) {
+            let int1 = b + i
+            result += int1
+    }
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = sumAll;
