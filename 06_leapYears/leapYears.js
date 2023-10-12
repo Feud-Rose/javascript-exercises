10a8eb0795ca83c0cdc9ea3de75a8ee0e49bf98f
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-12 09:37:21 -0600

    Complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+let leapCent = year % 100 
+let leapCheck = year % 4
+let leapCentCheck = year % 400
+if (leapCheck != 0) {
+return false
+}
+else if ((leapCent == 0) && (leapCentCheck !== 0)){
+    return false
+}
+else {
+    return true
+}
 };
-
+//if ((leapCheck = 0) && (leapCent != 0)) {
 // Do not edit below this line
 module.exports = leapYears;
