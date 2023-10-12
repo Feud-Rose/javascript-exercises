a2e42c5816c75232ed41de4b11a8c7a601729430
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-12 09:54:12 -0600

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+// Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32. 
+// Temperature in degrees Celsius (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9.
+
+const convertToCelsius = function(fahTemp) {
+  let num1 = (fahTemp - 32) * (5 / 9)
+  let tempInCel = Math.round(num1 * 10) / 10
+  return tempInCel
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  let num2 = (celTemp * ( 9 / 5 )) + 32
+  let tempInFah = Math.round( num2 * 10 ) / 10
+  return tempInFah
 };
 
 // Do not edit below this line
