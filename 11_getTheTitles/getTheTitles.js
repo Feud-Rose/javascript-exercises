732ce739b99d7ab5f25f86554a7821024573a3c6
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-26 14:11:56 -0600

    Complete 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let titles = obj.map(book => book.title)
+    return titles
+    
 };
 
 // Do not edit below this line
