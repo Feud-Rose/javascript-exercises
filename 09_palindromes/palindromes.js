abbfc8f88029cea8eb79a83f5f19c68ac9a77f0e
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-26 13:21:25 -0600

    Complete 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+const palindromes = function (word) {
+    let lower = word.toLowerCase();
+    let str = lower.split('') ;
+    
+    let clearedSpace = removeWhiteSpace(str)
+
+    punRemoved = clearedSpace.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1
+    })
+    let dupeArray = punRemoved.slice();
+    reversed = punRemoved.reverse()
+    if (dupeArray.join() == reversed.join()) {
+        return true
+    }
+    else { return false}
 
 };
+
+function removeWhiteSpace(array) {
+    return array.filter((item) => item != ' ');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
