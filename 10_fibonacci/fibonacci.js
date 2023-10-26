6e41ea5bcb0128a2c641b3a727d1ab283f2d5af3
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-26 13:52:46 -0600

    Complete 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+let myInt = parseInt(num)
+if (myInt < 0) {
+    return "OOPS"
+}
+else {
+        return Math.round(  
+        (((1 + Math.sqrt(5))/2) ** myInt - ((1 - Math.sqrt(5))/2) ** myInt) / Math.sqrt(5))
+    }
 };
 
 // Do not edit below this line
