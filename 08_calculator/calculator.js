fac36694ef3a4d0fefceaebfc4a0f0f11d38c3a6
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-26 11:16:55 -0600

    Complete 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = (a,b) => a + b 
+;
+
+const subtract = function(a,b) {
+	const subtracted = a - b
+  return subtracted
 };
 
-const subtract = function() {
-	
+const sum = function(sumIt) {
+  if (sumIt.length == 0) {
+    return 0
+  }
+  let result = sumIt.reduce((initial, cur) => initial + cur)
+	return parseInt(result)
 };
 
-const sum = function() {
-	
+const multiply = function(multiplyIt) {
+  let result = multiplyIt.reduce((initial, cur) => initial * cur, 1)
+	return parseInt(result)
+}
+
+const power = function(a,b) {
+  let toPower = Math.pow(a,b)
+  return toPower
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+  if (num === 0 || num === 1){ 
+    return 1
+  }
+  while (num > 1) {
+    num--;
+    result *= num; 
+  }
+  return result
 };
 
 // Do not edit below this line
