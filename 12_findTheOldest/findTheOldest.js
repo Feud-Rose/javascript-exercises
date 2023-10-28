ffd96da7308f465231777bc6c019a0355c6106df
Andrew <145285546+Feud-Rose@users.noreply.github.com>
2023-10-28 10:25:21 -0600

    Complete 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+   
+   console.log(Date)
+   console.log(obj)
+   
+    let sorted = obj.sort(function(a,b){
+      if (!a.yearOfDeath) {
+         let aAddDeath = addDeath(a);
+      }
+      if (!b.yearOfDeath) {
+         let bAddDeath = addDeath(b);
+      }
+
+      let varOne = a.yearOfDeath - a.yearOfBirth;
+       let varTwo = b.yearOfDeath - b.yearOfBirth;
+       return varOne > varTwo ? -1 : 1 ;
+
+    })
+    console.log(sorted)
+    const [firstItem, ...rest] = sorted
+    return firstItem
 
 };
 
+function addDeath(obj) {
+   let date = new Date
+   obj.yearOfDeath = date.getFullYear()
+   console.log({obj})
+}
 // Do not edit below this line
 module.exports = findTheOldest;
